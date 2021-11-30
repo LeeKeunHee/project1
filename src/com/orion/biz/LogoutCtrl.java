6141a62303d55a59f68c8c67296d7e73fd54d1c9
LeeKeunHee <dr_scv@naver.com>
2021-11-30 11:02:35 +0900

    11/30

---
+++ src/com/orion/biz/LogoutCtrl.java
@@ -0,0 +1,26 @@
+package com.orion.biz;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/LogoutCtrl")
+public class LogoutCtrl extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		String m_id = (String) session.getAttribute("m_id");
+		String name = (String) session.getAttribute("m_name");
+		if(m_id!="") {
+			session.invalidate();
+		}
+		response.sendRedirect("p_index.jsp");		
+	}
+
+}
